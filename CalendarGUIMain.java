720a584ab845b4b6452bc914029eb19133fb7bab
jtapang02 <jtapang02@gmail.com>
2014-08-12 05:14:51 +0000

    Created six conditional statements that present pop up windows detailing contextual error messages based on Christians validation methods. Changed Confidential to Default in classification since Google calender uses those three classifications. Changed "Public, Private, Default" to "PUBLIC, PRIVATE, DEFAULT" as Google doesn't read the lowercase versions of the strings. Changed return type of checkValidDate() method to int instead of boolean. This allowed me to have the method return numbers 1-4 for individual errors or 0 for success.

--- CalendarGUIMain.java
+++ CalendarGUIMain.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
 
-import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
-import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
-import net.sourceforge.jdatepicker.impl.UtilDateModel;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,262 +9,326 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class icaltemp extends JFrame implements ActionListener {
-  private JButton button1;
-  private JLabel label1;
-  private JLabel label2;
-  private JLabel label3;
-  private JLabel label4;
-  private JLabel label5;
-  private JLabel label6;
-  private JLabel label7;
-  private JLabel label8;
-  private JRadioButton radioButton1;
-  private JRadioButton radioButton2;
-  private JRadioButton radioButton3;
-  private JRadioButton radioButton4;
-  private JRadioButton radioButton5;
-  private JRadioButton radioButton6;
-  private JRadioButton radioButton7;
-  private JRadioButton radioButton8;
+public class CalendarGUIMain extends JFrame implements ActionListener {
+	private JButton button1;
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
+	private JLabel label4;
+	private JLabel label5;
+	private JLabel label6;
+	private JLabel label7;
+	private JLabel label8;
+	private JRadioButton radioButton1;
+	private JRadioButton radioButton2;
+	private JRadioButton radioButton3;
+	private JRadioButton radioButton4;
+	private JRadioButton radioButton5;
+	private JRadioButton radioButton6;
+	private JRadioButton radioButton7;
+	private JRadioButton radioButton8;
 
-  private JTextField textfield1;
-  private JTextField textfield2;
-  private JTextField textfield3;
-  private JTextField textfield4;
-  private JTextField textfield5;
-  // test things
-  private JSpinner dateSpinner;
-  // test things end
-  
-  //Moved classification and priority variables to main scope
-  private int pri;
-  private String classi;
-  
-  //new variables for calendar module
-  private String formattedDate;
-  private Date startD;
-  private Date endD;
-  private JDatePickerImpl dPicker;
-  // this constructor adds buttons, labels, and text fields
-  public icaltemp() {
+	private JTextField textfield1;
+	private JTextField textfield2;
+	private JTextField textfield3;
+	private JTextField textfield4;
+	private JTextField textfield5;
 
-    setLayout(new GridLayout(0, 1));
+	//Moved classification and priority variables to main scope
+	private int pri;
+	private String classi;
 
-    label1 = new JLabel("Event Name");
-    add(label1);
-    textfield1 = new JTextField(15);
-    add(textfield1);
+	//Constructor adds buttons, labels, and text fields
+	public CalendarGUIMain() {
 
-    label2 = new JLabel("Event Location");
-    add(label2);
-    textfield2 = new JTextField(15);
-    add(textfield2);
+		setLayout(new GridLayout(0, 1));
 
-    /*label3 = new JLabel("YYYYMMDD");
-    add(label3);
-    textfield3 = new JTextField(15);
-    add(textfield3);*/
-    
-    label3 = new JLabel("Event Date");
-    UtilDateModel udm = new UtilDateModel();
-    JDatePanelImpl dPanel = new JDatePanelImpl(udm);
-    dPicker = new JDatePickerImpl(dPanel);
-    add(dPicker);
-    startD = (Date)dPicker.getModel().getValue();
+		label1 = new JLabel("Event Name");
+		add(label1);
+		textfield1 = new JTextField(15);
+		add(textfield1);
 
-    label4 = new JLabel("Event Time Start");
-    add(label4);
-    textfield4 = new JTextField(15);
-    add(textfield4);
+		label2 = new JLabel("Event Location");
+		add(label2);
+		textfield2 = new JTextField(15);
+		add(textfield2);
 
-    label5 = new JLabel("Event Time End");
-    add(label5);
-    textfield5 = new JTextField(15);
-    add(textfield5);
+		label3 = new JLabel("YYYYMMDD");
+		add(label3);
+		textfield3 = new JTextField(15);
+		add(textfield3);
 
-    /*
-     * Still working on date spinner JSpinnerDate date = new Date();
-     * SpinnerDateModel dSpinner = new SpinnerDateModel(date, null, null,
-     * Calendar.DAY_OF_WEEK_IN_MONTH);dateSpinner = new
-     * JSpinner(dSpinner);//TEST SPINNERadd(dateSpinner);Still working on date
-     * spinner JSpinner
-     */
+		label4 = new JLabel("Event Time Start");
+		add(label4);
+		textfield4 = new JTextField(15);
+		add(textfield4);
 
-    // Priority High, Medium, Low
-    label6 = new JLabel("Priority");
-    add(label6);
-    pri = 5;
-    radioButton1 = new JRadioButton("High");
-    radioButton2 = new JRadioButton("Normal", true);
-    radioButton3 = new JRadioButton("Low");
-    add(radioButton1);
-    add(radioButton2);
-    add(radioButton3);
-    groupButton1();
+		label5 = new JLabel("Event Time End");
+		add(label5);
+		textfield5 = new JTextField(15);
+		add(textfield5);
 
-    // Is event recurring?
-    label7 = new JLabel("Recurring Event");
-    add(label7);
-    radioButton4 = new JRadioButton("Yes");
-    radioButton5 = new JRadioButton("No",true);
-    add(radioButton4);
-    add(radioButton5);
-    groupButton2();
+		// Priority High, Medium, Low
+		label6 = new JLabel("Priority");
+		add(label6);
+		pri = 5;
+		radioButton1 = new JRadioButton("HIGH");
+		radioButton2 = new JRadioButton("NORMAL", true);
+		radioButton3 = new JRadioButton("LOW");
+		add(radioButton1);
+		add(radioButton2);
+		add(radioButton3);
+		groupButton1();
 
-    // public,private,confidential
-    label8 = new JLabel("Event Visibility");
-    add(label8);
-    classi = "Public";
-    radioButton6 = new JRadioButton("Public", true);
-    radioButton7 = new JRadioButton("Private");
-    radioButton8 = new JRadioButton("Confidential");
-    add(radioButton6);
-    add(radioButton7);
-    add(radioButton8);
-    groupButton3();
+		//Recurring event related.
+		label7 = new JLabel("Recurring Event");
+		add(label7);
+		radioButton4 = new JRadioButton("YES");
+		radioButton5 = new JRadioButton("NO",true);
+		add(radioButton4);
+		add(radioButton5);
+		groupButton2();
 
-    // Button to create .ics file once previous inputs are filled.
-    button1 = new JButton("Create .ics");
-    add(button1);
-    button1.addActionListener(this);// Button is listening for event.
-  }
+		// public,private,confidential
+		label8 = new JLabel("Event Visibility");
+		add(label8);
+		classi = "Public";
+		radioButton6 = new JRadioButton("DEFAULT", true);
+		radioButton7 = new JRadioButton("PUBLIC");
+		radioButton8 = new JRadioButton("PRIVATE");
+		add(radioButton6);
+		add(radioButton7);
+		add(radioButton8);
+		groupButton3();
 
-  // Methods to group related radial buttons together.
-  public void groupButton1() {
-    ButtonGroup bg1 = new ButtonGroup();
-    bg1.add(radioButton1);
-    bg1.add(radioButton2);
-    bg1.add(radioButton3);
-    radioButton1.addItemListener(new HandlerClass(1));
-    radioButton2.addItemListener(new HandlerClass(5));
-    radioButton3.addItemListener(new HandlerClass(9));
-  }
+		// Button to create file once previous inputs are filled.
+		button1 = new JButton("Create .ics");
+		add(button1);
+		button1.addActionListener(this);// Button is listening for event.
+	}
 
-  public void groupButton2() {
-    ButtonGroup bg2 = new ButtonGroup();
-    bg2.add(radioButton4);
-    bg2.add(radioButton5);
-  }
+	//The following three methods group related radial buttons together.
+	public void groupButton1() {
+		ButtonGroup bg1 = new ButtonGroup();
+		bg1.add(radioButton1);
+		bg1.add(radioButton2);
+		bg1.add(radioButton3);
+		radioButton1.addItemListener(new HandlerClass(1));
+		radioButton2.addItemListener(new HandlerClass(5));
+		radioButton3.addItemListener(new HandlerClass(9));
+	}
 
-  public void groupButton3() {
-    ButtonGroup bg3 = new ButtonGroup();
-    bg3.add(radioButton6);
-    bg3.add(radioButton7);
-    bg3.add(radioButton8);
-    radioButton6.addItemListener(new HandlerClass2(radioButton6.getText()));
-    radioButton7.addItemListener(new HandlerClass2(radioButton7.getText()));
-    radioButton8.addItemListener(new HandlerClass2(radioButton8.getText()));
-  }
+	public void groupButton2() {
+		ButtonGroup bg2 = new ButtonGroup();
+		bg2.add(radioButton4);
+		bg2.add(radioButton5);
+	}
 
-  // Code relating to the creation of file after user is done and presses the
-  // button.
-  public void actionPerformed(ActionEvent e) {
+	public void groupButton3() {
+		ButtonGroup bg3 = new ButtonGroup();
+		bg3.add(radioButton6);
+		bg3.add(radioButton7);
+		bg3.add(radioButton8);
+		radioButton6.addItemListener(new HandlerClass2(radioButton6.getText()));
+		radioButton7.addItemListener(new HandlerClass2(radioButton7.getText()));
+		radioButton8.addItemListener(new HandlerClass2(radioButton8.getText()));
+	}
 
-    String locName = textfield2.getText();
-    String summary = textfield1.getText();
-    //String date = textfield3.getText();
-    
-    startD = (Date)dPicker.getModel().getValue();
-    DateFormat df = new SimpleDateFormat("yyyyMMdd");
-    formattedDate = df.format(startD);
-    
-    String recur;//
-    int clnum;//
-    String endt = textfield5.getText();
-    String startt = textfield4.getText();
-    int priority;//
-    TimeZone tz;
-    StringBuilder sb;
-    BufferedWriter writer = null;
-    tz = Calendar.getInstance().getTimeZone();
-    sb = new StringBuilder();
 
-    System.out.println("Event to be added:\n");
-    System.out.println("BEGIN:VCALENDAR");
-    sb.append("BEGIN:VCALENDAR\n");
-    System.out.println("PROID:-//Team Yangtze//yzcalendar 0.1//EN");
-    sb.append("PROID:-//Team Yangtze//yzcalendar 0.1//EN\n");
-    System.out.println("VERSION:1.0");
-    sb.append("VERSION:1.0\n");
-    System.out.println("BEGIN:VTIMEZONE");
-    sb.append("BEGIN:VTIMEZONE\n");
-    System.out.println("TZID:" + tz.getID());
-    sb.append("TZID:" + tz.getID() + "\n");
-    System.out.println("END:VTIMEZONE");
-    sb.append("END:VTIMEZONE\n");
-    System.out.println("BEGIN:VEVENT");
-    sb.append("BEGIN:VEVENT\n");
-    System.out.println("DTSTART:" + formattedDate + "T" + startt + "00Z");
-    sb.append("DTSTART:" + formattedDate + "T" + startt + "00Z\n");
-    System.out.println("DTEND:" + formattedDate + "T" + endt + "00Z");
-    sb.append("DTEND:" + formattedDate + "T" + endt + "00Z\n");
-    System.out.println("LOCATION:" + locName);
-    sb.append("LOCATION:" + locName + "\n");
-    System.out.println("SUMMARY:" + summary);
-    sb.append("SUMMARY:" + summary + "\n");
-    System.out.println("PRIORITY:"+pri);
-    sb.append("PRIORITY:"+pri+"\n");
-    System.out.println("CLASS:"+classi);
-    sb.append("CLASS:"+classi+"\n");
-    System.out.println("END:VEVENT");
-    sb.append("END:VEVENT\n");
-    System.out.println("END:VCALENDAR");
-    sb.append("END:VCALENDAR\n");
+	//Checks if users end time is after start time, if it is then return true, else return false.
+	//Compare startt and endt variables.
+	public boolean checkValidEndTime(String startTime, String endTime){
+		if(!endTime.matches("[0-9]+")|| endTime.length() < 4 || Integer.parseInt(endTime) > 2400 || Integer.parseInt(endTime) < Integer.parseInt(startTime))
+		{
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 
-    // IOException error may not apply with textfields and buttons.
-    try {
-      writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
-          "yangtze.ics"), "utf-8"));
-      writer.write(sb.toString());
-    } catch (IOException exception) {
-      System.out.println("Error writing to file");
-    } finally {
-      try {
-        writer.close();
-      } catch (Exception e2) {
-        System.out.println("Error writing to file");
-      }
-    }
-  }
-  //Private handler class for priority radio buttons
-  private class HandlerClass implements ItemListener
-  {
-    private int priority;
-    public HandlerClass(int p){
-      priority = p;
-    }
-    @Override
-    public void itemStateChanged(ItemEvent event) {
-      pri = priority;
-      
-    }
-  }
-  
-  //Private handler class for classification radio buttons
-  private class HandlerClass2 implements ItemListener
-  {
-    private String classification;
-    public HandlerClass2(String s){
-      classification = s;
-    }
-    @Override
-    public void itemStateChanged(ItemEvent event) {
-      classi = classification;
-      
-    }
-  }
-  public static void main(String args[]) {
-    CalendarGUIMain gui = new CalendarGUIMain();
-    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui.setSize(400, 620);
-    gui.setVisible(true);
-    gui.setTitle("Calendar Event Generator");
-  }
+	//Checks valid time if user inputs time that is crazy then return false else return true.
+	public static boolean checkValidTime(String userInput){
+		if(!userInput.matches("[0-9]+")|| userInput.length() < 4 || Integer.parseInt(userInput) > 2400)
+		{
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+
+	public static int checkValidDate(String date){//In form YYYYMMDD
+		String formattedDate;
+		Date current = new Date();
+		System.out.println(current.toString());
+		System.out.println(date.toString());
+
+		if(!date.matches("[0-9]+") || date.length() != 8)
+		{
+			return 1;
+		}
+
+		formattedDate = date.substring(4, 6) + "/" + date.substring(6, 8) + "/" + date.substring(0, 4);// MM/DD/YYYY
+
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		Date testDate = null;
+
+		try{
+			testDate = sdf.parse(formattedDate);
+		}
+		catch(ParseException pe){
+			System.out.println("The entered date was invalid.");
+			System.out.println("ERROR 2");//Added to see which error occurred.
+			return 2;
+		}
+		System.out.println("sdf: " + sdf.format(testDate));
+		System.out.println("formattedDate: " + formattedDate);
+		System.out.println(testDate);
+		System.out.println(date);
+		/*If the sdf.format(testDate) returns a different String than formattedDate string, the formatted date put into
+		 * the testDate is incorrect in some way. Ex 12/32/2014 becomes 1/1/2015*/
+
+		if(!(sdf.format(testDate).equals(formattedDate))){
+			System.out.println("The entered date was invalid.");
+			System.out.println("ERROR 3");//Added to see which error occurred.
+			return 3;
+		}	
+		if(current.after(testDate)){
+			System.out.println("The entered date is older than the current date.");
+			System.out.println("ERROR 4");//Added to see which error occurred.
+			return 4;
+		}
+		return 0;
+	}
+	// Code relating to the creation of file after user is done and presses the
+	// button.
+	public void actionPerformed(ActionEvent e) {
+		String locName = textfield2.getText();
+		String summary = textfield1.getText();
+		String date = textfield3.getText();
+		String recur;
+		String endt = textfield5.getText();
+		String startt = textfield4.getText();
+		TimeZone tz;
+		StringBuilder sb;
+		BufferedWriter writer = null;
+
+		tz = Calendar.getInstance().getTimeZone();
+		sb = new StringBuilder();
+
+		//Following six conditionals provide pop ups for errors that can occur when the user is inputting time and dates.
+		if(checkValidEndTime(startt, endt) == false){
+			JFrame frame = new JFrame("");
+			JOptionPane.showMessageDialog(frame, "ERROR: End time is before start time.", "Time Input Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else if(checkValidTime(startt) == false || checkValidTime(endt) == false){
+			JFrame frame = new JFrame("");
+			JOptionPane.showMessageDialog(frame, "ERROR: Time format is 24hr HHMM", "Time Input Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else if(checkValidDate(date) == 1){
+			JFrame frame = new JFrame("");
+			JOptionPane.showMessageDialog(frame, "ERROR: Date format is YYYYMMDD", "Date Input Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else if(checkValidDate(date) == 2){
+			JFrame frame = new JFrame("");
+			JOptionPane.showMessageDialog(frame, "ERROR: Date format is YYYYMMDD", "Date Input Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else if(checkValidDate(date) == 3){
+			JFrame frame = new JFrame("");
+			JOptionPane.showMessageDialog(frame, "ERROR: Invalid date, i.e. Feb 35th.", "Date Input Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else if(checkValidDate(date) == 4){
+			JFrame frame = new JFrame("");
+			JOptionPane.showMessageDialog(frame, "ERROR: Date precedes present.", "Date Input Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else{
+			System.out.println("Event to be added:\n");
+			System.out.println("BEGIN:VCALENDAR");
+			sb.append("BEGIN:VCALENDAR\n");
+			System.out.println("PROID:-//Team Yangtze//yzcalendar 0.1//EN");
+			sb.append("PROID:-//Team Yangtze//yzcalendar 0.1//EN\n");
+			System.out.println("VERSION:1.0");
+			sb.append("VERSION:1.0\n");
+			System.out.println("BEGIN:VTIMEZONE");
+			sb.append("BEGIN:VTIMEZONE\n");
+			System.out.println("TZID:" + tz.getID());
+			sb.append("TZID:" + tz.getID() + "\n");
+			System.out.println("END:VTIMEZONE");
+			sb.append("END:VTIMEZONE\n");
+			System.out.println("BEGIN:VEVENT");
+			sb.append("BEGIN:VEVENT\n");
+			System.out.println("DTSTART:" + date + "T" + startt + "00Z");
+			sb.append("DTSTART:" + date + "T" + startt + "00Z\n");
+			System.out.println("DTEND:" + date + "T" + endt + "00Z");
+			sb.append("DTEND:" + date + "T" + endt + "00Z\n");
+			System.out.println("LOCATION:" + locName);
+			sb.append("LOCATION:" + locName + "\n");
+			System.out.println("SUMMARY:" + summary);
+			sb.append("SUMMARY:" + summary + "\n");
+			System.out.println("PRIORITY:"+pri);
+			sb.append("PRIORITY:"+pri+"\n");
+			System.out.println("CLASS:"+classi);
+			sb.append("CLASS:"+classi+"\n");
+			System.out.println("END:VEVENT");
+			sb.append("END:VEVENT\n");
+			System.out.println("END:VCALENDAR");
+			sb.append("END:VCALENDAR\n");
+
+			// IOException error may not apply with text fields and buttons.
+			try {
+				writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
+						"yangtze.ics"), "utf-8"));
+				writer.write(sb.toString());
+			} catch (IOException exception) {
+				System.out.println("Error writing to file");
+			} finally {
+				try {
+					writer.close();
+				} catch (Exception e2) {
+					System.out.println("Error writing to file");
+				}
+			}
+		}
+	}
+	//Private handler class for priority radio buttons
+	private class HandlerClass implements ItemListener
+	{
+		private int priority;
+		public HandlerClass(int p){
+			priority = p;
+		}
+		@Override
+		public void itemStateChanged(ItemEvent event) {
+			pri = priority;
+
+		}
+	}
+
+	//Private handler class for classification radio buttons
+	private class HandlerClass2 implements ItemListener
+	{
+		private String classification;
+		public HandlerClass2(String s){
+			classification = s;
+		}
+		@Override
+		public void itemStateChanged(ItemEvent event) {
+			classi = classification;
+
+		}
+	}
+	public static void main(String args[]) {
+		CalendarGUIMain gui = new CalendarGUIMain();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setSize(400, 600);
+		gui.setVisible(true);
+		gui.setTitle("Calendar Event Generator");
+	}
 }
